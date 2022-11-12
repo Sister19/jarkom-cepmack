324297f2c37a3c7f4a7bd6aca6567dd178a9242d
firizky29 <13520095@std.stei.itb.ac.id>
2022-11-12 13:13:41 +0700

    feat: created segment class with calculate checksum method

--- lib/segment.py
+++ lib/segment.py
@@ -1,38 +1,70 @@
 import struct
 
 # Constants 
-SYN_FLAG = 0b0
-ACK_FLAG = 0b0
-FIN_FLAG = 0b0
+SYN_FLAG = 0b00000010
+ACK_FLAG = 0b00010000
+FIN_FLAG = 0b00000001
 
 class SegmentFlag:
     def __init__(self, flag : bytes):
         # Init flag variable from flag byte
-        pass
+        self.flag = flag
+        self.syn = bool(self.flag & SYN_FLAG)
+        self.ack = bool(self.flag & ACK_FLAG)
+        self.fin = bool(self.flag & FIN_FLAG)
 
     def get_flag_bytes(self) -> bytes:
         # Convert this object to flag in byte form
-        pass
+        return struct.pack("B", self.flag)
 
+    def toggle_syn(self):
+        # Toggle SYN flag
+        self.flag = self.flag ^ SYN_FLAG
+        self.syn = not self.syn
+    
+    def toggle_ack(self):
+        # Toggle ACK flag
+        self.flag = self.flag ^ ACK_FLAG
+        self.ack = not self.ack
 
-
-
+    def toggle_fin(self):
+        # Toggle FIN flag
+        self.flag = self.flag ^ FIN_FLAG
+        self.fin = not self.fin
 
 class Segment:
     # -- Internal Function --
     def __init__(self):
         # Initalize segment
-        pass
+        self.sequence = 0
+        self.ack = 0
+        self.flag = SegmentFlag(0b00000000)
+        self.checksum = 0
+        self.payload = b''
 
     def __str__(self):
         # Optional, override this method for easier print(segmentA)
         output = ""
         output += f"{'Sequence number':24} | {self.sequence}\n"
+        output += f"{'Acknowledgement number':24} | {self.ack}\n"
+        output += f"{'Flags':24} | [SYN: {self.flag.syn}] [ACK: {self.flag.ack}] [FIN: {self.flag.fin}]\n"
+        output += f"{'Checksum':24} | {self.checksum} [IS VALID: {self.valid_checksum()}]\n"
+        output += f"{'Data length':24} | {len(self.payload)}\n"
         return output
 
     def __calculate_checksum(self) -> int:
         # Calculate checksum here, return checksum result
-        pass
+        data = self.get_bytes_without_checksum()
+        for i in range(0, len(data), 2):
+            data_chunk = data[i:i+2]
+            if(len(data_chunk) == 1):
+                data_chunk += b'\x00'
+            self.checksum += struct.unpack("!H", data_chunk)[0]
+
+    
+        self.checksum = ~self.checksum & 0xFFFF
+        return self.checksum
+        
 
 
     # -- Setter --
@@ -64,10 +96,29 @@ class Segment:
 
     def get_bytes(self) -> bytes:
         # Convert this object to pure bytes
-        pass
+        res = b''
+        res += struct.pack("!I", self.sequence)
+        res += struct.pack("!I", self.ack)
+        res += self.flag.get_flag_bytes()
+        res += struct.pack("x")
+        res += struct.pack("!H", self.__calculate_checksum())
+        res += self.payload
+        return res
+
+    def get_bytes_without_checksum(self) -> bytes:
+        # Get bytes without checksum
+        res = b''
+        res += struct.pack("!I", self.sequence)
+        res += struct.pack("!I", self.ack)
+        res += self.flag.get_flag_bytes()
+        res += struct.pack("x") 
+        res += struct.pack("!H", 0x0000)
+        res += self.payload
+        return res
+
 
 
     # -- Checksum --
     def valid_checksum(self) -> bool:
         # Use __calculate_checksum() and check integrity of this object
-        pass
+        return self.__calculate_checksum() == 0x0000
