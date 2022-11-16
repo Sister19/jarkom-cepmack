16792ffbdef8a6aded470df97b6e85ef1b544936
Muhammad Fahmi Irfan <13520152@std.stei.itb.ac.id>
2022-11-16 16:02:40 +0700

    feat: three way handshek (untested)

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ from lib.args import Args
 from lib.segment import Segment
 import lib.segment
 import os, sys
-
+import signal
 
 # ini broadcast address kalau di windows, kalau di linux keknya bisa broadcast = "" aja;
 BROADCAST_ADDRESS = "255.255.255.255"
@@ -27,17 +27,40 @@ class Client:
         self.connection = Connection(self.ip, self.port, send_broadcast=True)
         self.connection.set_timeout(lib.config.CLIENT_LISTEN_TIMEOUT)
 
+    def _three_way_error(self):
+        raise Exception()
+
     def three_way_handshake(self):
-        # Three Way Handshake, client-side
-        server_request = Segment()
-        server_request.set_flag([lib.segment.SYN_FLAG])
+        signal.signal(signal.SIGALRM, self._three_way_error)
+        try:
+            signal.alarm(5)
+            # Hendshek pertama ngirim SYN
+            server_request = Segment()
+            server_request.set_flag([lib.segment.SYN_FLAG])
+            server_request.set_header({"sequence": 0, "ack" : 0})
 
-        # START OF NYOBAIN DOANG (DELETE THIS IF YOU ARE GOING TO USE THIS)
-        server_request.set_header({"sequence": 1, "ack": 1000})
-        # END OF NYOBAIN DOANG (DELETE THIS IF YOU ARE GOING TO USE THIS)
+            self.connection.send_data(server_request, (BROADCAST_ADDRESS, self.broadcast_port))
 
-        self.connection.send_data(server_request, (BROADCAST_ADDRESS, self.broadcast_port))
+            # Hendshek kedua nunggu SYN+ACK
+            segment_recv, (addr_recv, port_recv) = self.connection.listen_single_segment()
 
+            while(not segment_recv.valid_checksum()):
+                self.connection.send_data(server_request, (BROADCAST_ADDRESS, self.broadcast_port))
+                segment_recv, (addr_recv, port_recv) = self.connection.listen_single_segment()
+
+
+            # Hendshek ketiga ngirim ACK (kalo dapet)
+            ack_res = Segment()
+            ack_res.set_flag([lib.segment.ACK_FLAG])
+            ack_res.set_header({"sequence": 1, "ack" : segment_recv.get_header()["sequence"]+1})
+            self.connection.send_data(ack_res, (BROADCAST_ADDRESS, self.broadcast_port))
+        except:
+            print("tiga jalan goyang tangan")
+            print("untuk membuka jaringan")
+            print("gagal dijalankan")
+            print("sangat disayangkan")
+            
+            
     def listen_file_transfer(self):
         # File transfer, client-side
         pass
